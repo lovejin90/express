f55fbc2311a37ec36d2f15a47f26794e9b5cd866
mdugi <falcon@kakao.com>
2024-08-10 23:49:13 +0900

    test

--- routes/menu/index.js
+++ routes/menu/index.js
@@ -1,53 +1,65 @@
-var express = require('express');
+var express = require("express");
 const router = express.Router();
 
 const data = require("./data");
 const createError = require("http-errors");
-const {validationResult, check} = require("express-validator");
+const { validationResult, check } = require("express-validator");
 const mysql = require("../mysql");
 
-router.get('/getList',async function(req, res, next){
-    data.getMenuList((row)=>{ //유효성 검사에 적합하지 않으면 정보를 다시 조회 후, updateMemo 페이지를 다시 랜더링한다.
-        res.send(row);
-    });
+router.get("/getList", async function (req, res, next) {
+  data.getMenuList((row) => {
+    //유효성 검사에 적합하지 않으면 정보를 다시 조회 후, updateMemo 페이지를 다시 랜더링한다.
+    res.send(row);
+  });
 });
 
-router.post('/insertMemo', [check('content').isLength({min:1, max:500})], (req, res) =>{
+router.post(
+  "/insertMemo",
+  [check("content").isLength({ min: 1, max: 500 })],
+  (req, res) => {
     let errs = validationResult(req);
 
     let param = JSON.parse(JSON.stringify(req.body));
-    let id = param['id'];
-    let active_yn = param['active_yn'];
-    let name = param['name'];
+    let active_yn = param["active_yn"];
+    let url = param["url"];
+    let name = param["name"];
 
-    data.insertMemu(name, active_yn, (rlt) =>{
-        res.send(rlt);
+    data.insertMemu(name, active_yn, url, (rlt) => {
+      res.send(rlt);
     });
-});
-router.post('/updateMemo', [check('content').isLength({min:1, max:500})], (req, res) =>{
+  }
+);
+router.post(
+  "/updateMemo",
+  [check("content").isLength({ min: 1, max: 500 })],
+  (req, res) => {
     let errs = validationResult(req);
 
     let param = JSON.parse(JSON.stringify(req.body));
-    let id = param['id'];
-    let active_yn = param['active_yn'];
-    let name = param['name'];
+    let id = param["id"];
+    let active_yn = param["active_yn"];
+    let url = param["url"];
+    let name = param["name"];
 
-    data.updateMemu(id, name, active_yn, (rlt) =>{
-        res.send(rlt);
+    data.updateMemu(id, name, active_yn, url, (rlt) => {
+      res.send(rlt);
     });
-});
+  }
+);
 
-router.post('/deleteMemu', [check('content').isLength({min:1, max:500})], (req, res) =>{
+router.post(
+  "/deleteMemu",
+  [check("content").isLength({ min: 1, max: 500 })],
+  (req, res) => {
     let errs = validationResult(req);
 
     let param = JSON.parse(JSON.stringify(req.body));
-    let id = param['id'];
-    let active_yn = param['active_yn'];
-    let name = param['name'];
+    let id = param["id"];
 
-    data.deleteMemu(id, (rlt) =>{
-        res.send(rlt);
+    data.deleteMemu(id, (rlt) => {
+      res.send(rlt);
     });
-});
+  }
+);
 
 module.exports = router;
